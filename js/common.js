b5ffecf6d4c1bf56a29ee31e22a806dc2df8f6b3
Falldmoon <47499238+Falldmoon@users.noreply.github.com>
2020-02-14 02:28:51 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-  
+  let modal_window = $('.modal-window')[0]
+  let black_window = $('.black-window')[0]
+  $('.modal__exit').on('click', function() {
+  	$(modal_window).removeClass('modal-window_active');
+  	$(black_window).removeClass('black-window_active');
+  })
 });
